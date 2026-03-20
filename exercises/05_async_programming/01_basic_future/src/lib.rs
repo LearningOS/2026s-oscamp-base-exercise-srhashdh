6a25afe7b0477f71bb1fc5541e0d98697217b802
srhashdh <together219@gmail.com>
2026-03-20 10:50:57 +0800

    basic future

--- exercises/05_async_programming/01_basic_future/src/lib.rs
+++ exercises/05_async_programming/01_basic_future/src/lib.rs
@@ -33,7 +33,14 @@ impl Future for CountDown {
     type Output = &'static str;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        todo!()
+        if self.count == 0 {
+            Poll::Ready("liftoff!")
+        }else{
+            let task = self.get_mut();
+            task.count -= 1;
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
     }
 }
 
@@ -57,7 +64,14 @@ impl Future for YieldOnce {
     type Output = ();
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        todo!()
+        let task = self.get_mut();
+        if !task.yielded {
+            task.yielded = true;
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }else {
+            Poll::Ready(())
+        }
     }
 }
 
